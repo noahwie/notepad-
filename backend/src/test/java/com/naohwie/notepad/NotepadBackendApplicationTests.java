e10cce8724cd32754cbca8ae10ffc862d54d5f84
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-07-05 13:43:06 +0200

    created base code structure for backend, frontend and db

---
+++ backend/src/test/java/com/naohwie/notepad/NotepadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naohwie.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
