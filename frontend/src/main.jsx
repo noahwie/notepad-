f0bb834e6ab514bdfe4757f1836e6815795e391f
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-07-10 14:48:23 +0200

    renamed Main to MainPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MainPage from "./pages/Main.jsx";
+import MainPage from "./pages/MainPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
